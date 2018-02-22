53d200776f58c62377a26f2a1da9e28a98ee70fa
Joshua Scheel <jscheel42@gmail.com>
2018-02-22 11:47:59 -0500

    update foreign key for AbaModel.Map -> :replays

---
+++ clean_deps.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mix deps.clean --all && mix deps.get
--- lib/aba_model/map.ex
+++ lib/aba_model/map.ex
@@ -9,7 +9,7 @@ defmodule AbaModel.Map do
     field :name, :string
 
     has_many :translations, AbaModel.MapTranslation, on_delete: :delete_all
-    has_many :replays, AbaModel.Replay
+    has_many :replays, AbaModel.Replay, foreign_key: :game_map_id
   end
 
   def changeset(map, params \\ %{}) do
